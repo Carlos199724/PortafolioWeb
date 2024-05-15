b9f2d13ce15455387310b8b8a52edcdcbb76db2a
Carlos199724 <73734421+Carlos199724@users.noreply.github.com>
2024-05-14 21:15:16 -0500

    modificacion Cv

--- curriculum.js
+++ curriculum.js
@@ -1,3 +1,3 @@
 document.getElementById("descargar-cv").addEventListener("click", function() {
-    window.open("doc/MI_CURRICULUMOFICIAL.pdf", "_blank");
+    window.open("doc/CURRICULUM_OFICIAL.pdf", "_blank");
 });

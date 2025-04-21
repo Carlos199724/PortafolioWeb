2a3447e730a80f990e8937c391993764a29a06d0
cgoicocheam <cgoicocheam@autonoma.edu.pe>
2025-04-21 13:10:31 -0500

    update

--- curriculum.js
+++ curriculum.js
@@ -1,3 +1,3 @@
 document.getElementById("descargar-cv").addEventListener("click", function() {
-    window.open("doc/CURRICULUM_OFICIAL.pdf", "_blank");
+    window.open("doc/CURRICULUM_PRO.pdf", "_blank");
 });
